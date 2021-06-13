7ebee40157551a990f3c67274d6ae9070fb7b63b
Enzo <enzocuellar12@gmail.com>
2021-06-12 21:31:15 -0300

    app setup ready

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="bg-black text-white text-center text-3xl">
+        Habit tracker
+      </h1>
+      ;
+    </div>
+  );
+}
