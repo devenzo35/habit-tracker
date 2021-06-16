3cb29bd0c4cae5fcba3e173eeee3d900159c26f2
Enzo <enzocuellar12@gmail.com>
2021-06-16 10:49:44 -0300

    add draggable reorder list (not finished yet)

--- components/HabitsContainer.tsx
+++ components/HabitsContainer.tsx
@@ -1,12 +1,31 @@
 import HabitCard from "./HabitCard";
+import { Droppable } from "react-beautiful-dnd";
+import { useEffect, useState } from "react";
+
 const HabitsContainer = () => {
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3006/notes")
+      .then((res) => res.json())
+      .then((json) => setNotes(json));
+  }, []);
+
   return (
-    <div className="bg-gray-800 border-gray-700 rounded-md flex flex-col justify-around border gap-2 w-5/12 h-3/6 p-5">
-      <HabitCard />
-      <HabitCard />
-      <HabitCard />
-      <HabitCard />
-    </div>
+    <Droppable droppableId={"1"}>
+      {(provided) => (
+        <div
+          className="bg-gray-800 border-gray-700 rounded-md flex flex-col justify-around border gap-2 w-5/12 h-3/6 p-5"
+          {...provided.droppableProps}
+          ref={provided.innerRef}
+        >
+          {notes.map((note, index) => {
+            return <HabitCard key={note.id} {...note} index={index} />;
+          })}
+          {provided.placeholder}
+        </div>
+      )}
+    </Droppable>
   );
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,15 +4,35 @@ import React from "react";
 import styles from "../styles/Home.module.css";
 import HabitsContainer from "../components/HabitsContainer";
 import HabitsTracker from "../components/HabitsTracker";
+import { DragDropContext } from "react-beautiful-dnd";
 
 export default function Home() {
+  const onDragEnd = (result) => {
+    const { destination, source, draggableId } = result;
+
+    if (!destination) {
+      return;
+    }
+
+    if (
+      destination.draggableId === source.draggableId ||
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    const column = "1";
+
+    const newHabitsList = Array.from(column.taskId);
+  };
   return (
     <div className="bg-gray-900 h-screen flex flex-row items-center justify-around">
       <h1 className="bg-black w-full text-white text-center text-3xl fixed top-0">
         Habit tracker
       </h1>
-
-      <HabitsContainer />
+      <DragDropContext onDragEnd={onDragEnd}>
+        <HabitsContainer />
+      </DragDropContext>
       <HabitsTracker />
     </div>
   );
