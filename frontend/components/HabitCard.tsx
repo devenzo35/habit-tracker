10c0ad7006c3e235e9f3e2df4cac02b23700af53
Enzo <enzocuellar12@gmail.com>
2021-08-09 10:36:27 -0300

    add strapi for backend

---
+++ frontend/components/HabitCard.tsx
@@ -0,0 +1,26 @@
+import { Draggable } from "react-beautiful-dnd";
+
+const HabitCard = ({ id, name, done, index }) => {
+  return (
+    <Draggable draggableId={id.toString()} index={index}>
+      {(provided) => (
+        <figure
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
+        >
+          <div className="bg-gray-600 text-gray-300 border-gray-700 rounded-3xl border  group w-full h-16 flex pl-4 flex-row items-center justify-center relative duration-500 hover:bg-gray-800 hover:pr-32">
+            <strong>
+              <span className="text-3xl m-auto">{name}</span>
+            </strong>
+            <button className="bg-yellow-500 text-gray-800 text-3xl rounded-r-3xl absolute w-3/12 right-0 h-full opacity-0 group-hover:opacity-100 duration-500 hover:bg-yellow-400">
+              <strong>DONE</strong>
+            </button>
+          </div>
+        </figure>
+      )}
+    </Draggable>
+  );
+};
+
+export default HabitCard;
